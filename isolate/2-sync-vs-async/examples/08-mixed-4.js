39896eca7f2b1bc3d2b7116b1b3136f672f4ed99
ERKAM GURESEN <erkamguresen@gmail.com>
2021-08-09 03:22:34 -0700

    path errors, and a function param error are fixed

--- isolate/2-sync-vs-async/examples/08-mixed-4.js
+++ isolate/2-sync-vs-async/examples/08-mixed-4.js
@@ -1,18 +1,18 @@
-import { labeledLogger } from '../../../lib/labeled-logger.js';
-import { synchronousTimeout } from '../../lib/synchronous-timeout.js';
+import { labeledLogger } from "../../../lib/labeled-logger.js";
+import { synchronousTimeout } from "../../../lib/synchronous-timeout.js";
 
 const log = labeledLogger(Date.now());
 
-log('+ schedule async 1000 ms');
-setTimeout(log, 1000, '+ execute async  1000 ms');
+log("+ schedule async 1000 ms");
+setTimeout(log, 1000, "+ execute async  1000 ms");
 
-log('+ schedule async 0 ms');
-setTimeout(log, 0, '+ execute async  0 ms');
+log("+ schedule async 0 ms");
+setTimeout(log, 0, "+ execute async  0 ms");
 
-log('- begin synchronous 500 ms');
-synchronousTimeout(log, 500, '- end synchronous 500 ms');
+log("- begin synchronous 500 ms");
+synchronousTimeout(log, 500, "- end synchronous 500 ms");
 
-log('- begin synchronous 0 ms');
-synchronousTimeout(log, 0, '- end synchronous 0 ms');
+log("- begin synchronous 0 ms");
+synchronousTimeout(log, 0, "- end synchronous 0 ms");
 
-log('= = = =  the call stack is empty  = = = =');
+log("= = = =  the call stack is empty  = = = =");
